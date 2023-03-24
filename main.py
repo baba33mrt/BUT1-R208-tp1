6b98e5fd1f7f9b009133b9ecd1b08187efc0a6e5
baba33mrt <baptiste18032004@gmail.com>
2023-03-24 13:57:52 +0100

    reformat the file

--- main.py
+++ main.py
@@ -19,8 +19,11 @@ Type    --> str         | Type de donnée
 [OUTPUT]
 tab     --> list        | Tableau contenant les données
 """
+
+
 def add(array, date, hour, Id, value, type):
     array.append([checkDate(date), checkHeure(hour), Id, value, type])
+
 
 # Fonction qui affiche le tableau
 def viewTab(array):
@@ -29,7 +32,7 @@ def viewTab(array):
     while i < len(array):
         j = 0
         if i != 0:
-            if array[i][0] != array[i-1][0]:
+            if array[i][0] != array[i - 1][0]:
                 string = string + '\nDate\t\t\tHeure\t\t\tCapteur\t\t\tValeur\t\t\tType\n'
         while j < len(array[i]):
             string = string + f'{array[i][j]}\t\t'
@@ -37,7 +40,6 @@ def viewTab(array):
         i = i + 1
         string = string + '\n'
     print(string)
-
 
 
 # Fonction qui vérifie le format de la date
@@ -49,6 +51,7 @@ def checkDate(date):
     print("Format de date invalide")
     return -1
 
+
 # Fonction qui vérifie le format de l'heure
 def checkHeure(heure):
     h, m, s = heure
@@ -58,10 +61,12 @@ def checkHeure(heure):
     print("Format de d'heure invalide")
     return -1
 
+
 def sortDate(date1, date2):
     if date1 < date2:
         return True
     return False
+
 
 def sortDateDESC(date1, date2):
     if date1 > date2:
@@ -79,6 +84,7 @@ def getBetweenDate(array, date1, date2):
         i = i + 1
     return tab
 
+
 # Fonction qui renvoie la valeur minimale d'un type de donnée
 def getMin(array, type):
     tmparray = []
@@ -88,6 +94,7 @@ def getMin(array, type):
             tmparray.append(array[i][3])
         i = i + 1
     return min([tmparray])
+
 
 # Fonction qui renvoie la valeur maximale d'un type de donnée
 def getMax(array, type):
@@ -99,6 +106,7 @@ def getMax(array, type):
         i = i + 1
     return max([tmparray])
 
+
 # Fonction qui renvoie la valeur maximale d'un type de donnée
 def getMoy(array, type):
     tmparray = []
@@ -109,8 +117,9 @@ def getMoy(array, type):
             tmparray.append(array[i])
             moy = moy + array[i][3]
         i = i + 1
-        moy = moy/len(tmparray)
+        moy = moy / len(tmparray)
     return moy
+
 
 # fonction qui trie le tableau par date
 def sortByDate(array):
@@ -122,16 +131,9 @@ def sortByDate(array):
                 tmp = array[i]
                 array[i] = array[j]
                 array[j] = tmp
-            j = j+1
-        i=i+1
+            j = j + 1
+        i = i + 1
     return array
-
-
-
-
-
-
-
 
 
 add(array, (2020, 10, 5), (11, 27, 40), 5, 22.1, 'TEMP')
@@ -139,20 +141,22 @@ add(array, (2019, 3, 9), (11, 27, 40), 5, 22.1, 'TEMP')
 add(array, (2021, 1, 10), (11, 27, 40), 5, 22.1, 'TEMP')
 add(array, (2028, 10, 3), (11, 27, 40), 5, 22.1, 'TEMP')
 
-
-
 # main #
 
+# Menu principal où l'utilisateur peut choisir ce qu'il veut faire.
 while True:
-    print("Bonjour, Quel action souhaitez vous faire ?\n1) Ajouter de nouvelles valeurs dans le tableau\n2) Trier les valeurs du tableau par la date la plus récente\n3) Afficher le tabelau\n4) Afficher la valeur max\n5) Afficher la valeur min\n6) Afficher la valeur moyenne\n0) Quitter\n >> ")
+    print(
+        "Bonjour, Quel action souhaitez vous faire ?\n1) Ajouter de nouvelles valeurs dans le tableau\n2) Trier les valeurs du tableau par la date la plus récente\n3) Afficher le tabelau\n4) Afficher la valeur max\n5) Afficher la valeur min\n6) Afficher la valeur moyenne\n0) Quitter\n >> ")
     value = int(input())
-    if value == 1:
+    if value == 1:  # test si la date est valide
         test = 0
         while test == 0:
-            date = (int(input("Merci d'indiquer les valeurs suivantes :\nAnnée : ")), int(input("Mois : ")), int(input('Jour : ')))
+            date = (int(input("Merci d'indiquer les valeurs suivantes :\nAnnée : ")), int(input("Mois : ")),
+                    int(input('Jour : ')))
             date = checkDate(date)
             if date != -1:
                 test = 1
+
         test = 0
         while test == 0:
             heure = (int(input("Heure : ")), int(input("Minute : ")), int(input('Seconde : ')))
@@ -179,5 +183,3 @@ while True:
 
     elif value == 0:
         break
-
-
